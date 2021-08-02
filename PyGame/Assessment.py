f2de3947a4b0c6fe533f323c502fab011a52b969
Nathan-web24 <78878767+Nathan-web24@users.noreply.github.com>
2021-08-02 20:40:25 +1200

    My new

--- PyGame/Assessment.py
+++ PyGame/Assessment.py
@@ -1,115 +1,288 @@
 import os, time, pygame
-# Load our scenes
-from states.title import Title
 
+#Create State
+class State():
+    def __init__(self, game):
+        self.game = game
+        self.prev_state = None
+
+    def update(self, delta_time, actions):
+        pass
+    def render(self, surface):
+        pass
+
+    def enter_state(self):
+        if len(self.game.state_stack) > 1:
+            self.prev_state = self.game.state_stack[-1]
+        self.game.state_stack.append(self)
+
+    def exit_state(self):
+        self.game.state_stack.pop()
+
+#Load our scene
+class Title(State):
+    def __init__(self, game):
+        State.__init__(self, game)
+
+    def update(self, delta_time, actions):
+        if actions["start"]:
+            new_state = Game_World(self.game)
+            new_state.enter_state()
+        self.game.reset_keys()
+
+    def render(self, display):
+        display.fill((255, 255, 255))
+        self.game.draw_text(display, "Welcome to my first game", (0, 0, 0), self.game.GAME_W / 2, self.game.GAME_H / 2)
+
+#Create Player
+class Player():
+    def __init__(self, game):
+        self.game = game
+        self.load_sprites()
+        self.position_x, self.position_y = 200, 200
+        self.current_frame, self.last_frame_update = 0, 0
+
+    def update(self, delta_time, actions):
+        # Get the direction from inputs
+        direction_x = actions["right"] - actions["left"]
+        direction_y = actions["down"] - actions["up"]
+        # Update the position
+        self.position_x += 100 * delta_time * direction_x
+        self.position_y += 100 * delta_time * direction_y
+        # Animate the sprite
+        self.animate(delta_time, direction_x, direction_y)
+
+    def render(self, display):
+        display.blit(self.curr_image, (self.position_x, self.position_y))
+
+    def animate(self, delta_time, direction_x, direction_y):
+        # Compute how much time has passed since the frame last updated
+        self.last_frame_update += delta_time
+        # If no direction is pressed, set image to idle and return
+        if not (direction_x or direction_y):
+            self.curr_image = self.curr_anim_list[0]
+            return
+        # If an image was pressed, use the appropriate list of frames according to direction
+        if direction_x:
+            if direction_x > 0:
+                self.curr_anim_list = self.right_sprites
+            else:
+                self.curr_anim_list = self.left_sprites
+        if direction_y:
+            if direction_y > 0:
+                self.curr_anim_list = self.front_sprites
+            else:
+                self.curr_anim_list = self.back_sprites
+        # Advance the animation if enough time has elapsed
+        if self.last_frame_update > .15:
+            self.last_frame_update = 0
+            self.current_frame = (self.current_frame + 1) % len(self.curr_anim_list)
+            self.curr_image = self.curr_anim_list[self.current_frame]
+
+    def load_sprites(self):
+        # Get the diretory with the player sprites
+        self.sprite_dir = os.path.join(self.game.sprite_dir, "player")
+        self.front_sprites, self.back_sprites, self.right_sprites, self.left_sprites = [], [], [], []
+        # Load in the frames for each direction
+        for i in range(1, 5):
+            self.front_sprites.append(
+                pygame.image.load(os.path.join(self.sprite_dir, "player_front" + str(i) + ".png")))
+            self.back_sprites.append(pygame.image.load(os.path.join(self.sprite_dir, "player_back" + str(i) + ".png")))
+            self.right_sprites.append(
+                pygame.image.load(os.path.join(self.sprite_dir, "player_right" + str(i) + ".png")))
+            self.left_sprites.append(pygame.image.load(os.path.join(self.sprite_dir, "player_left" + str(i) + ".png")))
+        # Set the default frames to facing front
+        self.curr_image = self.front_sprites[0]
+        self.curr_anim_list = self.front_sprites
+#Create Party Menu
+class PartyMenu(State):
+    def __init__(self, game):
+        self.game = game
+        State.__init__(self, game)
+
+    def update(self, delta_time, actions):
+        if actions["action2"]:
+            self.exit_state()
+        self.game.reset_keys()
+
+    def render(self, display):
+        display.fill((255,255,255))
+        self.game.draw_text(display, "PARTY MENU GOES HERE", (0,0,0), self.game.GAME_W/2, self.game.GAME_H/2 )
+
+#Create Pause Menu
+class PauseMenu(State):
+    def __init__(self, game):
+        self.game = game
+        State.__init__(self, game)
+        # Set the menu
+        self.menu_img = pygame.image.load(os.path.join(self.game.assets_dir, "map", "menu.png"))
+        self.menu_rect = self.menu_img.get_rect()
+        self.menu_rect.center = (self.game.GAME_W*.85, self.game.GAME_H*.4)
+        # Set the cursor and menu states
+        self.menu_options = {0 :"Party", 1 : "Items", 2 :"Magic", 3 : "Exit"}
+        self.index = 0
+        self.cursor_img = pygame.image.load(os.path.join(self.game.assets_dir, "map", "cursor.png"))
+        self.cursor_rect = self.cursor_img.get_rect()
+        self.cursor_pos_y = self.menu_rect.y + 38
+        self.cursor_rect.x, self.cursor_rect.y = self.menu_rect.x + 10, self.cursor_pos_y
+
+    def update(self, delta_time, actions):
+        self.update_cursor(actions)
+        if actions["action1"]:
+            self.transition_state()
+        if actions["action2"]:
+            self.exit_state()
+        self.game.reset_keys()
+
+    def render(self, display):
+        # render the gameworld behind the menu, which is right before the pause menu on the stack
+        #self.game.state_stack[-2].render(display)
+        self.prev_state.render(display)
+        display.blit(self.menu_img, self.menu_rect)
+        display.blit(self.cursor_img, self.cursor_rect)
+
+    def transition_state(self):
+        if self.menu_options[self.index] == "Party":
+            new_state = PartyMenu(self.game)
+            new_state.enter_state()
+        elif self.menu_options[self.index] == "Items":
+            pass # TO-DO
+        elif self.menu_options[self.index] == "Magic":
+            pass # TO-DO
+        elif self.menu_options[self.index] == "Exit":
+            while len(self.game.state_stack) > 1:
+                self.game.state_stack.pop()
+
+
+    def update_cursor(self, actions):
+        if actions['down']:
+            self.index = (self.index + 1) % len(self.menu_options)
+        elif actions['up']:
+            self.index = (self.index - 1) % len(self.menu_options)
+        self.cursor_rect.y = self.cursor_pos_y + (self.index * 32)
+
+#Create World
+class Game_World(State):
+    def __init__(self, game):
+        State.__init__(self, game)
+        self.player = Player(self.game)
+        self.grass_img = pygame.image.load(os.path.join(self.game.assets_dir, "map", "grass.png"))
+
+    def update(self, delta_time, actions):
+        # Check if the game was paused
+        if actions["start"]:
+            new_state = PauseMenu(self.game)
+            new_state.enter_state()
+        self.player.update(delta_time, actions)
+
+    def render(self, display):
+        display.blit(self.grass_img, (0, 0))
+        self.player.render(display)
+
+# create control
 class Game():
-#create control
-        def __init__(self):
-            pygame.init()
-            self.GAME_W,self.GAME_H = 480, 270
-            self.SCREEN_WIDTH,self.SCREEN_HEIGHT = 960, 540
-            self.game_canvas = pygame.Surface((self.GAME_W,self.GAME_H))
-            self.screen = pygame.display.set_mode((self.SCREEN_WIDTH,self.SCREEN_HEIGHT))
-            self.running, self.playing = True, True
-            self.actions = {"left": False, "right": False, "up" : False, "down" : False, "action1" : False, "action2" : False, "start" : False}
-            self.dt, self.prev_time = 0, 0
-            self.state_stack = []
-            self.load_assets()
-            self.load_states()
+    def __init__(self):
+        pygame.init()
+        self.GAME_W, self.GAME_H = 480, 270
+        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = 960, 540
+        self.game_canvas = pygame.Surface((self.GAME_W, self.GAME_H))
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.running, self.playing = True, True
+        self.actions = {"left": False, "right": False, "up": False, "down": False, "action1": False, "action2": False,
+                        "start": False}
+        self.dt, self.prev_time = 0, 0
+        self.state_stack = []
+        self.load_assets()
+        self.load_states()
 
-        def game_loop(self):
-            while self.playing:
-                self.get_dt()
-                self.get_events()
-                self.update()
-                self.render()
+    def game_loop(self):
+        while self.playing:
+            self.get_dt()
+            self.get_events()
+            self.update()
+            self.render()
 
-        def get_events(self):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+    def get_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
                     self.playing = False
                     self.running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.playing = False
-                        self.running = False
-                    if event.key == pygame.K_a:
-                        self.actions['left'] = True
-                    if event.key == pygame.K_d:
-                        self.actions['right'] = True
-                    if event.key == pygame.K_w:
-                        self.actions['up'] = True
-                    if event.key == pygame.K_s:
-                        self.actions['down'] = True
-                    if event.key == pygame.K_p:
-                        self.actions['action1'] = True
-                    if event.key == pygame.K_o:
-                        self.actions['action2'] = True
-                    if event.key == pygame.K_RETURN:
-                        self.actions['start'] = True
+                if event.key == pygame.K_a:
+                    self.actions['left'] = True
+                if event.key == pygame.K_d:
+                    self.actions['right'] = True
+                if event.key == pygame.K_w:
+                    self.actions['up'] = True
+                if event.key == pygame.K_s:
+                    self.actions['down'] = True
+                if event.key == pygame.K_p:
+                    self.actions['action1'] = True
+                if event.key == pygame.K_o:
+                    self.actions['action2'] = True
+                if event.key == pygame.K_RETURN:
+                    self.actions['start'] = True
 
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_a:
-                        self.actions['left'] = False
-                    if event.key == pygame.K_d:
-                        self.actions['right'] = False
-                    if event.key == pygame.K_w:
-                        self.actions['up'] = False
-                    if event.key == pygame.K_s:
-                        self.actions['down'] = False
-                    if event.key == pygame.K_p:
-                        self.actions['action1'] = False
-                    if event.key == pygame.K_o:
-                        self.actions['action2'] = False
-                    if event.key == pygame.K_RETURN:
-                        self.actions['start'] = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    self.actions['left'] = False
+                if event.key == pygame.K_d:
+                    self.actions['right'] = False
+                if event.key == pygame.K_w:
+                    self.actions['up'] = False
+                if event.key == pygame.K_s:
+                    self.actions['down'] = False
+                if event.key == pygame.K_p:
+                    self.actions['action1'] = False
+                if event.key == pygame.K_o:
+                    self.actions['action2'] = False
+                if event.key == pygame.K_RETURN:
+                    self.actions['start'] = False
 
-#create timer
-        def time(self):
-            
+    def update(self):
+        self.state_stack[-1].update(self.dt, self.actions)
 
+    def render(self):
+        self.state_stack[-1].render(self.game_canvas)
+        # Render current states to the screen
+        self.screen.blit(pygame.transform.scale(self.game_canvas, (self.SCREEN_WIDTH, self.SCREEN_HEIGHT)), (0, 0))
+        pygame.display.flip()
 
+    def get_dt(self):
+        now = time.time()
+        self.dt = now - self.prev_time
+        self.prev_time = now
 
+    def draw_text(self, surface, text, color, x, y):
+        text_surface = self.font.render(text, True, color)
+        # text_surface.set_colorkey((0,0,0))
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x, y)
+        surface.blit(text_surface, text_rect)
 
-        def update(self):
-            self.state_stack[-1].update(self.dt,self.actions)
+    def load_assets(self):
+        # Create pointers to directories
+        self.assets_dir = os.path.join("assets")
+        self.sprite_dir = os.path.join(self.assets_dir, "sprites")
+        self.font_dir = os.path.join(self.assets_dir, "font")
+        self.font = pygame.font.Font(os.path.join(self.font_dir, "PressStart2P-vaV7.ttf"), 20)
 
-        def render(self):
-            self.state_stack[-1].render(self.game_canvas)
-            # Render current states to the screen
-            self.screen.blit(pygame.transform.scale(self.game_canvas,(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)), (0,0))
-            pygame.display.flip()
+    def load_states(self):
+        self.title_screen = Title(self)
+        self.state_stack.append(self.title_screen)
 
+    def reset_keys(self):
+        for action in self.actions:
+            self.actions[action] = False
 
-        def get_dt(self):
-            now = time.time()
-            self.dt = now - self.prev_time
-            self.prev_time = now
-
-        def draw_text(self, surface, text, color, x, y):
-            text_surface = self.font.render(text, True, color)
-            #text_surface.set_colorkey((0,0,0))
-            text_rect = text_surface.get_rect()
-            text_rect.center = (x, y)
-            surface.blit(text_surface, text_rect)
-
-        def load_assets(self):
-            # Create pointers to directories
-            self.assets_dir = os.path.join("assets")
-            self.sprite_dir = os.path.join(self.assets_dir, "sprites")
-            self.font_dir = os.path.join(self.assets_dir, "font")
-            self.font= pygame.font.Font(os.path.join(self.font_dir, "PressStart2P-vaV7.ttf"), 20)
-
-        def load_states(self):
-            self.title_screen = Title(self)
-            self.state_stack.append(self.title_screen)
-
-        def reset_keys(self):
-            for action in self.actions:
-                self.actions[action] = False
-
-
+#Run
 if __name__ == "__main__":
     g = Game()
     while g.running:
         g.game_loop()
+
+
+
